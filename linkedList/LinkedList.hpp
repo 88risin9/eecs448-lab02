d8c2752d924d0dc2fbc7afb132cbf353135b62f2
Zhenzhou Wang <z715w673@ku.edu>
2022-02-08 10:18:51 -0600

    fix removeback

--- linkedList/LinkedList.hpp
+++ linkedList/LinkedList.hpp
@@ -1,6 +1,6 @@
 /**
-*	@author 
-*	@date 
+*	@author
+*	@date
 *	@file LinkedList.hpp
 *	@brief Implementation file for templated LinkedList class
 */
@@ -12,7 +12,7 @@ LinkedList<T>::LinkedList() : m_front(nullptr), m_size(0)
 }
 
 template <typename T>
-LinkedList<T>::~LinkedList() 
+LinkedList<T>::~LinkedList()
 {
 	while(!isEmpty())
 	{
@@ -29,10 +29,11 @@ bool LinkedList<T>::isEmpty() const
 template <typename T>
 int LinkedList<T>::size() const
 {
-	/** TODO 
+	/** TODO
 		Fix this method
 	*/
-	return(0);
+
+	return m_size ;
 }
 
 template <typename T>
@@ -41,9 +42,21 @@ bool LinkedList<T>::search(T value) const
 	Node<T>* temp = m_front;
 	bool isFound = false;
 
-	/** TODO 
+	/** TODO
 		Fix this method
 	*/
+	while(temp != nullptr)
+	{
+		if(temp->getValue()==value)
+		{
+			isFound=true;
+			return(isFound);
+		}
+		else
+		{
+			temp=temp->getNext();
+		}
+	}
 
 	return(isFound);
 }
@@ -60,8 +73,8 @@ std::vector<T> LinkedList<T>::toVector() const
 		temp = temp->getNext();
 	}
 
-	return(vec);	
-	
+	return(vec);
+
 }
 
 template <typename T>
@@ -71,7 +84,7 @@ void LinkedList<T>::addBack(T value)
 
 	if(isEmpty())
 	{
-		m_front = new Node<T>(value);	
+		m_front = new Node<T>(value);
 	}
 	else
 	{
@@ -81,7 +94,7 @@ void LinkedList<T>::addBack(T value)
 			temp = temp->getNext();
 		}
 
-		temp->setNext( new Node<T>(value) );		
+		temp->setNext( new Node<T>(value) );
 	}
 
 	m_size++;
@@ -103,12 +116,67 @@ bool LinkedList<T>::removeBack()
 	Node<T>* secondintoLast = nullptr;
 	bool isRemoved = false;
 
-	/** TODO 
+	/** TODO
 		Fix this method
 	*/
 
+	if(m_size==1)
+	{
+	lastNode=m_front;
+	delete lastNode;
+	m_size=0;
+	return true;
+	}
+
+	secondintoLast=m_front;
+	lastNode = m_front->getNext();
+
+
+	//find second last node
+	if(m_size!=1)
+	{
+	while(lastNode->getNext()!=nullptr)
+	{
+		secondintoLast=secondintoLast->getNext();
+		lastNode = lastNode->getNext();
+
+	}
+	//delete last Node
+	delete lastNode;
+
+	m_size--; 
+
+	//change secondintoLast point to nullptr
+	secondintoLast->setNext(nullptr);
+	isRemoved=true;
+}
+/*
+if(m_size==1)
+{
+lastNode=m_front;
+delete lastNode;
+m_size=0;
+return true;
+}
+
+if(m_size!=1)
+{
+	lastNode=m_front ->getNext();
+	secondintoLast=m_front;
+	for(int i=0; i<m_size-2;++i)
+	{
+		lastNode=lastNode->getNext();
+		secondintoLast=secondintoLast->getNext();
+
+	}
+	delete lastNode;
+	secondintoLast->setNext(nullptr);
+	m_size--;
+	isRemoved=true;
+}
+*/
 	return(isRemoved);
-}	
+}
 
 template <typename T>
 bool LinkedList<T>::removeFront()
